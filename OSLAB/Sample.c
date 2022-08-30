e09aa83557519f571ba498c981f8d6a593a98a57
JANARDHANA <94514945+204g1a0541@users.noreply.github.com>
2022-08-30 11:14:29 +0530

    Create Sample.c
    L Janardhana Created Sample.c"

---
+++ OSLAB/Sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: G.Janardhana
+   Roll No: 204G1A0541
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
